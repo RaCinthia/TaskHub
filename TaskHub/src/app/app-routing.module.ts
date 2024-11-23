35d148acc915cbc21ad2df61d034f56c4b59bf81
Radrígyla Fonseca <radrigylafonseca@gmail.com>
2024-11-23 17:18:50 +0000

    router-outlet adicionado

--- TaskHub/src/app/app-routing.module.ts
+++ TaskHub/src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { Login2Component } from './pages/login2/login2.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 const routes: Routes = [
+  {path: "landing-page", component: LandingPageComponent},
   {path: "login", component: LoginComponent},
   {path: "login2", component: Login2Component},
   {path: 'home', component: HomePageComponent},
