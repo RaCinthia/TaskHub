637c968b87cac6c03df13448e82db802a82f1778
Rá <raschool123@gmail.com>
2024-11-28 23:09:25 -0300

    Animação landing-page

--- TaskHub/src/app/pages/landing-page/landing-page.component.ts
+++ TaskHub/src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+          entry.target.classList.add('show');
+          entry.target.classList.forEach((names) => {
+            if(names == 'hidden-left') {
+              entry.target.classList.add('from-left');;
+            } else if(names == 'hidden-right') {
+              entry.target.classList.add('from-right');
+            };
+          });
+        } else {
+          entry.target.classList.remove('show');
+        };
+      });
+    });
+
+    const elements = document.querySelectorAll('.hidden');
+    elements.forEach((element) => observer.observe(element));
+  }
+  
 }
